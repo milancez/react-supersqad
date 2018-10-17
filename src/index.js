84ffda085577590f702d741f8d944b80349f7423
milancez <milancez@gmail.com>
2018-10-17 14:58:28 +0200

    Create redux actions and reducers - characters_reducer, heroes_reducer, helpers

--- src/index.js
+++ src/index.js
@@ -1,12 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import App from './components/App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+import rootReducer from './reducers';
+import { addCharacterById } from './actions';
+
+const store = createStore(rootReducer);
+console.log('store.getState()', store.getState());
+store.subscribe(() => console.log('store', store.getState()));
+store.dispatch(addCharacterById(2));
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>, 
+  document.getElementById('root')
+);
