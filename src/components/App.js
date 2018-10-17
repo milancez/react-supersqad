d0a48054ba8b3cb83a58479fc6a164867788715c
milancez <milancez@gmail.com>
2018-10-17 16:34:12 +0200

    connect, mapStateToProps, mapDispatchToProps, CharacterList component, HeroList component

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react';
+import CharacterList from './CharacterList';
+import HeroList from './HeroList';
+import '../styles/index.css';
 
 class App extends Component {
   state = {  }
   render() { 
     return ( 
-      <div>
+      <div className="App">
         <h2>SuperSquad</h2>
+        <div className="col-md-6">
+          <CharacterList />
+        </div>
+        <div className="col-md-6">
+          <HeroList />
+        </div>
       </div>
      );
   }
